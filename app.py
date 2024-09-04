f229ffb471751d07e6a7e06c977fa290e67686b9
Jiawen <jiawen27.chua@gmail.com>
2024-09-04 17:55:41 +0800

    Reapply "Merge branch 'main' of https://github.com/FutureYY/Budgeting"
    This reverts commit 5e27584d4197d62bbf952611b4e04b2f2616c803.

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from os import path
+
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from app.config import Config
 from app.models import db
 from app import init_bp
-from flask_login import LoginManager
-from app.auth import auth
-from app.views import views
-
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -22,16 +19,6 @@ def create_app(config_class=Config):
         if not path.exists(app.config['DATABASE_NAME']):
             db.create_all()
             print('Created Database!')
-
-        # Login Manager
-        Manager_Login = LoginManager()
-        Manager_Login.login_view = "auth.login"
-        Manager_Login.init_app(app)
-
-        @Manager_Login.user_loader
-        def user_load(id):
-            return Users.query.get(id)
-
 
     # Register blueprints
     app.register_blueprint(init_bp)
