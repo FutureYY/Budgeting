a14abf97e66a18ef3c0ef8abaaa72555591bc570
Yoshana18 <yoshana.magendran.18@gmail.com>
2024-09-05 20:11:00 +0800

    Revert "Update app.py"
    This reverts commit 7bca8821d0d8d1f237495f870a9892f47b16ef37.

--- app.py
+++ app.py
@@ -22,10 +22,6 @@ def create_app(config_class=Config):
             db.create_all()
             print('Created Database!')
             # insert_dummy_data()
-<<<<<<< Updated upstream
-=======
-            # users_check()
->>>>>>> Stashed changes
 
     from app.models import User
 
@@ -44,7 +40,6 @@ def create_app(config_class=Config):
     return app
 
 
-<<<<<<< Updated upstream
 # def insert_dummy_data():
 #     # Check if the user already exists
 #     existing_user = User.query.filter_by(email='testuser@example.com').first()
@@ -69,39 +64,6 @@ def create_app(config_class=Config):
 #         print('Inserted Dummy Data!')
 #     else:
 #         print('User already exists, skipping dummy data insertion.')
-=======
-def insert_dummy_data():
-    # Check if the user already exists
-    existing_user = User.query.filter_by(email='testuser@example.com').first()
-
-    if not existing_user:
-        user = User(name='testuser', email='testuser@example.com')
-        db.session.add(user)
-        db.session.commit()
-
-        income1 = Income(amount=1000.00, category='salary', user_id=user.id)
-        income2 = Income(amount=200.00, category='allowance', user_id=user.id)
-        db.session.add_all([income1, income2])
-
-        expense1 = Expense(amount=50.00, category='food_beverages', user_id=user.id)
-        expense2 = Expense(amount=30.00, category='transport', user_id=user.id)
-        db.session.add_all([expense1, expense2])
-
-        budget = Budget(month='2024-09', income_goal=1500.00, savings_goal=500.00, expense_goal=1000.00,
-                        user_id=user.id)
-        db.session.add(budget)
-
-        transaction1 = Transaction(amount=1000.00, category='salary', type='income', date=date.today(), user_id=user.id,
-                                   budget_id=budget.id)
-        transaction2 = Transaction(amount=50.00, category='food_beverages', type='expense', date=date.today(),
-                                   user_id=user.id, budget_id=budget.id)
-        db.session.add_all([transaction1, transaction2])
-
-        db.session.commit()
-        print('Inserted Dummy Data!')
-    else:
-        print('User already exists, skipping dummy data insertion.')
->>>>>>> Stashed changes
 
 if __name__ == '__main__':
     app = create_app()
